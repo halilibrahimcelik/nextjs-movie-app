9f664b90a12bef2b48205047a482793297d039bd
halilibrahimcelik <halil.celik139@gmail.com>
2023-10-29 12:39:28 +0300

    feat:fonts and global styling  added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
-import Image from "next/image";
-
 export default function Home() {
-  return <main></main>;
+  return (
+    <main className="container">
+      <div className="row">
+        <p>Hello</p>
+      </div>
+      <h2>Lorem ipsum dolor sit amet.</h2>
+    </main>
+  );
 }
